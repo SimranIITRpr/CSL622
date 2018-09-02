c7b3fee01476532d171484891b1ac4df003f17b7
Prinshu Kumar <2016csb1051@iitrpr.ac.in>
2018-09-02 09:58:10 +0530

    Update and rename removeLeastImpressive to removeLeastImpressive_assign2.py

--- removeLeastImpressive_assign2.py
+++ removeLeastImpressive_assign2.py
@@ -13,12 +13,14 @@ G.remove_node(35)
 G.remove_node(40)
 #nx.draw(G,with_labels=1)
 y = []
+
 for i in range(33):
       if i == 17:
          continue
       p = G.in_edges(i+1)
       x = len(p)
       y.append(x)
+      
 p = G.in_edges(100)
 y.append(len(p))
 
@@ -27,3 +29,14 @@ y.sort()
 print (y)
 print ("Required edges : ")
 print (y[1]-y[0])
+
+list2 = G.in_edges(y[0])
+nodesList = G.nodes()
+count = 0
+
+for i in nodesList:
+	if i not in list2:
+		print i
+		count+=1
+	if count==y[1]-y[0]:
+		break
